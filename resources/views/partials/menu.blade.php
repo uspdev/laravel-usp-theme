596aa1edc84dd7bdf866b609a149fc9d19b43e07
Masaki Kawabata Neto <masaki.kawabata@gmail.com>
2019-06-06 14:37:19 -0300

    ajuste na url do dashboard

--- resources/views/partials/menu.blade.php
+++ resources/views/partials/menu.blade.php
@@ -2,7 +2,7 @@
 
 <nav class="navbar navbar-expand-sm navbar-light bg-light mb-3 py-0 border-bottom border-top border-gray">
     <div class="container">
-        <a class="navbar-brand" href="/"> <strong>
+        <a class="navbar-brand" href="{{ $dashboard_url }}"> <strong>
                 @section('sitename')
                 {{ $title }}
                 @show

3796b8a514b28f1c75adfecb3c775651a6cce179
Masaki Kawabata Neto <masaki.kawabata@gmail.com>
2019-06-06 15:59:49 -0300

    de novo hehe

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -37,10 +37,12 @@
                         @auth
                         {{ Auth::user()->name }} - {{ Auth::user()->email }} |
                         @if ( $logout_method == 'POST' )
-                        <form action="{{ $logout_url }}" method="POST" class="form-inline" style="display:inline-block">
+                        <form action="{{ $logout_url }}" method="POST" class="form-inline" style="display:inline-block"
+                            id="logout_form">
                             {{ csrf_field() }}
                             <!-- O uso do link ao invés do botao é para poder formatar corretamente -->
-                            <a onclick="document.getElementById('logout_form').submit(); return false;" class="font-weight-bold text-white nounderline pr-2 pl-2" href>Sair</a>
+                            <a onclick="document.getElementById('logout_form').submit(); return false;"
+                                class="font-weight-bold text-white nounderline pr-2 pl-2" href>Sair</a>
                         </form>
                         @else
                         <a class="font-weight-bold text-white nounderline pr-2 pl-2" href="{{ $logout_url }}">Sair</a>

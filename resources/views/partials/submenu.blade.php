965707983c77e5b5716d454b2dcdc77bf7b49f86
Masaki Kawabata Neto <kawabata@usp.br>
2020-08-26 19:48:47 -0300

    ajeitando submenus

--- resources/views/partials/submenu.blade.php
+++ resources/views/partials/submenu.blade.php
@@ -1,24 +1,19 @@
-<li class="nav-item">
+<li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown{{ preg_replace('/[^\w\d]+/', '', $item['text']) }}"
+        role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        {{ $item['text'] }}
+    </a>
     <div class="dropdown">
-
-      <button class="btn dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        {{$item['text']}}
-      </button>
-
-      <div class="dropdown-menu">
-        @foreach ($item['submenu'] as $submenu_item)
-          @isset($submenu_item['can'])
-            @if (Gate::check($submenu_item['can']))
-              <button class="dropdown-item" type="button">
-              <a href="{{ $submenu_item['url'] }}"> {{$submenu_item['text']}} </a>
-              </button>
-            @endif
-          @else
-            <button class="dropdown-item" type="button">
-            <a href="{{ $submenu_item['url'] }}"> {{$submenu_item['text']}} </a>
-            </button>
-         @endisset
-        @endforeach
-      </div>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown{{ preg_replace('/[^\w\d]+/', '', $item['text']) }}">
+            @foreach ($item['submenu'] as $submenu_item)
+                @isset($submenu_item['can'])
+                    @if (Gate::check($submenu_item['can']))
+                        <a class="dropdown-item" href="{{ $submenu_item['url'] }}"> {{ $submenu_item['text'] }} </a>
+                    @endif
+                @else
+                    <a class="dropdown-item" href="{{ $submenu_item['url'] }}"> {{ $submenu_item['text'] }} </a>
+                @endisset
+            @endforeach
+        </div>
     </div>
 </li>

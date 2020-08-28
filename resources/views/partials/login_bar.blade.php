b54774077cc972602468d8b502114ede9bf1369b
Masaki Kawabata Neto <kawabata@usp.br>
2020-08-28 17:32:54 -0300

    icones em login e logout

--- resources/views/partials/login_bar.blade.php
+++ resources/views/partials/login_bar.blade.php
@@ -6,14 +6,21 @@
     <form action="{{ $logout_url }}" method="POST" class="form-inline" style="display:inline-block" id="logout_form">
         {{ csrf_field() }}
         <!-- O uso do link ao invés do botao é para poder formatar corretamente -->
-        <a onclick="document.getElementById('logout_form').submit(); return false;" class="font-weight-bold text-white nounderline pr-2 pl-2" href>Sair</a>
+        <a onclick="document.getElementById('logout_form').submit(); return false;"
+            class="font-weight-bold text-white nounderline pr-2 pl-2" href>
+            <i class="fas fa-sign-out-alt"></i> Sair
+        </a>
     </form>
     @else
-    <a class="font-weight-bold text-white nounderline pr-2 pl-2" href="{{ $logout_url }}">Sair</a>
+    <a class="font-weight-bold text-white nounderline pr-2 pl-2" href="{{ $logout_url }}">
+        <i class="fas fa-sign-out-alt"></i> Sair
+    </a>
     @endif
     @else
     Não autenticado |
-    <a class="font-weight-bold text-white nounderline pr-2 pl-2" href="{{ $login_url }}">Entrar</a>
+    <a class="font-weight-bold text-white nounderline pr-2 pl-2" href="{{ $login_url }}">
+        <i class="fas fa-sign-in-alt"></i> Entrar
+    </a>
     @endauth
 
 </div>

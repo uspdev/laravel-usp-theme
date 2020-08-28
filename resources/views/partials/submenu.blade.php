9f304229c3084d594755562a85f79b8ba330f208
Masaki Kawabata Neto <kawabata@usp.br>
2020-08-27 22:40:16 -0300

    icones nos menus

--- resources/views/partials/submenu.blade.php
+++ resources/views/partials/submenu.blade.php
@@ -1,17 +1,17 @@
 <li class="nav-item dropdown">
     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown{{ preg_replace('/[^\w\d]+/', '', $item['text']) }}"
         role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        {{ $item['text'] }}
+        {!! $item['text'] !!}
     </a>
     <div class="dropdown">
         <div class="dropdown-menu" aria-labelledby="navbarDropdown{{ preg_replace('/[^\w\d]+/', '', $item['text']) }}">
             @foreach ($item['submenu'] as $submenu_item)
                 @isset($submenu_item['can'])
                     @if (Gate::check($submenu_item['can']))
-                        <a class="dropdown-item" href="{{ $submenu_item['url'] }}"> {{ $submenu_item['text'] }} </a>
+                        <a class="dropdown-item" href="{{ $submenu_item['url'] }}"> {!! $submenu_item['text'] !!} </a>
                     @endif
                 @else
-                    <a class="dropdown-item" href="{{ $submenu_item['url'] }}"> {{ $submenu_item['text'] }} </a>
+                    <a class="dropdown-item" href="{{ $submenu_item['url'] }}"> {!! $submenu_item['text'] !!} </a>
                 @endisset
             @endforeach
         </div>

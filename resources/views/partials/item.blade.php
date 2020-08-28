812258d6025ff44fb83279f94f8f86e9ed4a620e
Masaki Kawabata Neto <kawabata@usp.br>
2020-08-28 16:52:03 -0300

    aplicando para submenus

--- resources/views/partials/item.blade.php
+++ resources/views/partials/item.blade.php
@@ -1,9 +1,8 @@
 <li class="nav-item {{ request()->url() == $item['url'] ? 'active' : '' }} ">
     <a class="nav-link" 
-        href="{{ $item['url'] }}" 
-        title="{{ isset($item['title']) ? $item['title'] : strip_tags($item['text']) }}"
-        target="{{ isset($item['target']) ? $item['target'] : '' }}"
-    >
+    href="{{ $item['url'] }}" 
+    title="{{ isset($item['title']) ? $item['title'] : strip_tags($item['text']) }}"
+    target="{{ isset($item['target']) ? $item['target'] : '' }}">
         {!! $item['text'] !!}
     </a>
 </li>

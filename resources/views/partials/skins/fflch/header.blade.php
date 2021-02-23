0d1dcdd384b50b6614f685cca40d2174f7f21f00
Thiago Gomes Verissimo <verissimotgv@gmail.com>
2021-02-22 23:36:29 -0300

    Improving FFLCH theme

--- resources/views/partials/skins/fflch/header.blade.php
+++ resources/views/partials/skins/fflch/header.blade.php
@@ -27,12 +27,12 @@
 <div class="container-fluid d-none d-sm-block">
     <div class="row">
         <div class="col-md-12">
-            <a class="skin_logo float-left" href="http://sti.fflch.usp.br/">
+            <a class="skin_logo float-left" href="{{ config('app.url')}}">
                 <img src="{{ asset('/vendor/laravel-usp-theme/skins/fflch/images/logo_fflch.png') }}" alt="Logo da Universidade de São Paulo" />
             </a>
             <div class="float-left pr-1 pt-2">
-                <div>Faculdade de Filosofia, Letras e Ciências Humanas</div>
-                <div class="small">Seção técnica de Informática</div>
+                <div>{{ config('app.name')}} </div>
+                <div class="small">Faculdade de Filosofia, Letras e Ciências Humanas</div>
             </div>
             <div class="skin_logo  float-right pr-1 pt-2">
                 <img src="{{ asset('/vendor/laravel-usp-theme/skins/fflch/images/usp.png') }}" alt="Logo da Universidade de São Paulo" />

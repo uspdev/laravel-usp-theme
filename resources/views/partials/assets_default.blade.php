80c34b5754e6232e0d1eb9ec33000f2fbc585a1d
Alessandro Costa de Oliveira <alecosta.web@gmail.com>
2021-03-04 17:01:16 -0300

    Removido cdn JQuery antigo

--- resources/views/partials/assets_default.blade.php
+++ resources/views/partials/assets_default.blade.php
@@ -30,7 +30,6 @@
     @section('javascripts_default')
 
     {{-- Jquery --}}
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
     {{-- Jquery --}}
 

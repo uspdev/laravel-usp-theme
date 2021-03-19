719bbe28dd918feda0818fdd33f2ad09bc817d31
Leandro Ramos <leandroramos@disroot.org>
2021-03-18 21:29:48 -0300

    Altera header do skin da ECA

--- resources/views/partials/skins/eca/header.blade.php
+++ resources/views/partials/skins/eca/header.blade.php
@@ -28,14 +28,14 @@
     <div class="row">
         <div class="col-md-12">
             <a class="skin_logo float-left" href="{{ config('app.url')}}">
-                <img src="{{ asset('/vendor/laravel-usp-theme/skins/fflch/images/logo_fflch.png') }}" alt="Logo da Universidade de São Paulo" />
+                <img src="{{ asset('/vendor/laravel-usp-theme/skins/eca/images/logo_eca.png') }}" alt="Logo da ECA - Escola de Comunicações e Artes" />
             </a>
             <div class="float-left pr-1 pt-2">
                 <div>{{ config('app.name')}} </div>
-                <div class="small">Faculdade de Filosofia, Letras e Ciências Humanas</div>
+                <div class="small">Escola de Comunicações e Artes</div>
             </div>
             <div class="skin_logo  float-right pr-1 pt-2">
-                <img src="{{ asset('/vendor/laravel-usp-theme/skins/fflch/images/usp.png') }}" alt="Logo da Universidade de São Paulo" />
+                <img src="{{ asset('/vendor/laravel-usp-theme/skins/eca/images/usp.png') }}" alt="Logo da Universidade de São Paulo" />
             </div>
         </div>
     </div>

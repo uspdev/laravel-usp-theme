53f6ed208283552714905024ae01f0ef92ca8753
Lucas Flóro <lucas@fearp.usp.br>
2021-09-20 11:48:54 -0300

    Criação de skin para fea-rp.

---
+++ resources/views/partials/skins/fearp/footer.blade.php
@@ -0,0 +1,9 @@
+@section('skin_footer')
+<!-- Footer uspdev -->
+<footer>
+    <div class="text-center py-4">
+        <hr />
+        <a href="https://www.fearp.usp.br/" target="_blank">fea-RP - {{ now()->year }}</a>
+    </div>
+</footer>
+@endsection

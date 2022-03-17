0beb72bc2a0977e381f3f1dbe4cc989746ed3f28
Masaki Kawabata Neto <kawabata@usp.br>
2022-03-17 10:47:21 -0300

    hotfix revertendo arquivo alterado incorretamente

--- resources/views/partials/skins/fflch/menu/item.blade.php
+++ resources/views/partials/skins/fflch/menu/item.blade.php
@@ -1,10 +1,4 @@
-@if (isset($item['url']))
-  <a class="nav-link nav-item {{ $item['class'] }}" href="{{ $item['url'] }}" title="{{ $item['title'] }}"
-    target="{{ $item['target'] }}">
-    {!! $item['text'] !!}
-  </a>
-@else
-  <span class="nav-link nav-item {{ $item['class'] }}" title="{{ $item['title'] }}" target="{{ $item['target'] }}">
-    {!! $item['text'] !!}
-  </span>
-@endif
+<a class="nav-link nav-item {{ $item['class'] }}" href="{{ $item['url'] }}" title="{{ $item['title'] }}"
+  target="{{ $item['target'] }}">
+  {!! $item['text'] !!}
+</a>

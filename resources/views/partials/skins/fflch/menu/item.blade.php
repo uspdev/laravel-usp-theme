1ac15008ebb6ccab7d8ed05cc31cffeb0d28ddb1
Thiago Gomes Verissimo <verissimotgv@gmail.com>
2022-03-20 14:07:50 -0300

    correções no skin fflch

--- resources/views/partials/skins/fflch/menu/item.blade.php
+++ resources/views/partials/skins/fflch/menu/item.blade.php
@@ -1,4 +1,19 @@
-<a class="nav-link nav-item {{ $item['class'] }}" href="{{ $item['url'] }}" title="{{ $item['title'] }}"
-  target="{{ $item['target'] }}">
-  {!! $item['text'] !!}
+<a class="nav-link nav-item {{ $item['class'] }}"
+
+  @isset($item['url'])
+    href="{{ $item['url'] }}"
+  @endisset
+
+  @isset($item['title'])
+    title="{{ $item['title'] }}"
+  @endisset
+
+  @isset($item['target'])
+    target="{{ $item['target'] }}"
+  @endisset
+  >
+
+  @isset($item['text'])
+    {!! $item['text'] !!}
+  @endisset
 </a>

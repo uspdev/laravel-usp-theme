f970ecccc1bd64f6bba32cdec9c09b9050998cc1
Thiago Gomes Verissimo <verissimotgv@gmail.com>
2025-01-18 10:18:24 -0300

    Nas mensagens de flash, permitir html
    Muitas vezes ao dispararmos mensagens flash, necessitamos dar ênfase em alguma parte da mesagem de erro, usando listas ul/ol, negrito, itálico, ou mesmo css inline, assim, como são partes do template controladas por quem está programando, não é um risco deixar escapar as tags html nessa região.

--- resources/views/partials/content_flash.blade.php
+++ resources/views/partials/content_flash.blade.php
@@ -2,7 +2,7 @@
     <div class="alert alert-danger">
         <ul>
             @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
+                <li>{!! $error !!}</li>
             @endforeach
         </ul>
     </div>
@@ -11,7 +11,7 @@
 <div class="flash-message">
     @foreach (['danger', 'warning', 'success', 'info'] as $msg)
         @if (Session::has('alert-' . $msg))
-            <p class="alert alert-{{ $msg }}">{{ Session::get('alert-' . $msg) }}
+            <p class="alert alert-{{ $msg }}">{!! Session::get('alert-' . $msg) !!}
                 <a href="#" class="close" data-dismiss="alert" aria-label="fechar">&times;</a>
             </p>
         @endif

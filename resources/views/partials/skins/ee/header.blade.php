24e6f05f8b22a31a9c4031a6e7fa93afd33592c2
JhonathaNunes <jhonathasilveira@gmail.com>
2025-09-16 11:55:39 -0300

    fix: atualiza referencias a de eeusp para ee

--- resources/views/partials/skins/ee/header.blade.php
+++ resources/views/partials/skins/ee/header.blade.php
@@ -1,33 +1,33 @@
 @section('skin_styles')
     @parent {{-- devemos incluir o conteúdo existente --}} 
-    <link rel="stylesheet" property="stylesheet"  href="{{ asset('/vendor/laravel-usp-theme/skins/eeusp/css/style.css')}}">
+    <link rel="stylesheet" property="stylesheet"  href="{{ asset('/vendor/laravel-usp-theme/skins/ee/css/style.css')}}">
 @endsection 
 
 @section('skin_header')
-    <div class="eeusp-logodefault">
+    <div class="ee-logodefault">
 
-        <div class="eeusp-firstsecondthird">
+        <div class="ee-firstsecondthird">
 
-            <div class="eeusp-logowrap">
-                <div class="eeusp-item">
-                    <a class="eeusp-logo-link" href="{{ config('app.url')}}">
-                    <img class="eeusp-logo-img" src="{{ asset('/vendor/laravel-usp-theme/skins/eeusp/images/logo-eeusp.png') }}">
+            <div class="ee-logowrap">
+                <div class="ee-item">
+                    <a class="ee-logo-link" href="{{ config('app.url')}}">
+                    <img class="ee-logo-img" src="{{ asset('/vendor/laravel-usp-theme/skins/ee/images/logo-eeusp.png') }}">
                     </a>
                 </div>
             </div>
 
-            <div class="eeusp-second">       
-                <div class="eeusp-item">
-                    <div class="eeusp-site-name">
-                        <a class="eeusp-site-name-link" href="{{ config('app.url')}}">{{ config('app.name')}} </a>
+            <div class="ee-second">       
+                <div class="ee-item">
+                    <div class="ee-site-name">
+                        <a class="ee-site-name-link" href="{{ config('app.url')}}">{{ config('app.name')}} </a>
                     </div>
                 </div>
             </div>
             
-            <div class="eeusp-third">
-                <div class="eeusp-item">
-                    <a class="eeusp-logo-usp-link" href="https://www.usp.br" target="_blank">
-                        <img class="eeusp-logo-img" src="{{ asset('/vendor/laravel-usp-theme/skins/eeusp/images/usp.png') }}" alt="Logo da Universidade de São Paulo">
+            <div class="ee-third">
+                <div class="ee-item">
+                    <a class="ee-logo-usp-link" href="https://www.usp.br" target="_blank">
+                        <img class="ee-logo-img" src="{{ asset('/vendor/laravel-usp-theme/skins/ee/images/usp.png') }}" alt="Logo da Universidade de São Paulo">
                     </a>  
                 </div>
             </div>

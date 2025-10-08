fc4311cd5d845ebe27bd5ad4630d93667c46b7fa
Masaki Kawabata Neto <kawabata@usp.br>
2025-10-08 17:08:13 -0300

    feat: data-top para offset do stick

--- resources/views/blocos/sticky.blade.php
+++ resources/views/blocos/sticky.blade.php
@@ -7,8 +7,10 @@ Usando a cor do background padrão do card do bootstrap, sem transparência
 Uso:
 - Incluir no layouts.app ou em outro lugar: @include('laravel-usp-theme::blocos.sticky')
 - Adiconar a classe 'card-header-sticky'
+- adicionar data-top="X" se quiser um top diferente de 0 (ex: para evitar sobreposição com navbar fixa)
 
 @author Masakik, em 31/3/2023
+@author Kawabata, em 8/10/2025: adicionado data-top para evitar sobreposição com navbar fixa
 --}}
 @section('styles')
   @once
@@ -23,5 +25,21 @@ Uso:
         background-color: #F0F0F0;
       }
     </style>
+  @endonce
+@endsection
+
+@section('javascripts_bottom')
+  @once
+    @parent
+    <script>
+      document.addEventListener('DOMContentLoaded', function() {
+        document.querySelectorAll('.card-header-sticky').forEach(function(el) {
+          if (el.hasAttribute('data-top')) {
+            const top = el.getAttribute('data-top');
+            el.style.top = `${top}px`;
+          }
+        });
+      });
+    </script>
   @endonce
 @endsection

d86903411ccb0bb95f6cf816016585143bcdd1f0
fkacuta <fkacuta@gmail.com>
2026-09-15 11:16:26 -0300

    Adição da skin da FO

---
+++ resources/views/partials/skins/fo/footer.blade.php
@@ -0,0 +1,9 @@
+@section('skin_footer')
+<!-- Footer uspdev -->
+<footer>
+    <div class="text-center py-4">
+        <hr />
+        <a href="https://www.fo.usp.br/" target="_blank">© {{ now()->year }} FO</a>
+    </div>
+</footer>
+@endsection
